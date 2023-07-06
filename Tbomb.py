039c21b1ad8ae049e9f90aacae5c09545ae7324d
MD Seam <118121199+Sh-Seam@users.noreply.github.com>
2023-07-06 23:06:12 +0600

    Create Tbomb.py

---
+++ Tbomb.py
@@ -0,0 +1,14 @@
+https://raw.githubusercontent.com/Toxic-Noob/ToxicBomber/main/more/.version")
+    
+     ToxicBomber && git clone https://github.com/Toxic-Noob/ToxicBomber > /dev/null 2>&1")
+        
+         
+    print("\033[94m│ \033[95mAuthor : ToxicNoob Inc.                \033[94m│".center(columns+15))
+    print("│ \033[95mTool   : Unlimited SMS Bomber          \033[94m│".center(columns+9))
+    print("│ \033[95mGitHub : https://github.com/Toxic-Noob \033[94m│".center(columns+9))
+    print("│ \033[95mCoder  : HunterSl4d3              \033[37mV4.1 \033[94m│".center(columns+15))
+    print("\033[94m└────────────────────────────────────────┘".center(columns+5))
+
+
+
+
