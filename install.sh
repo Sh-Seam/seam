b0a98a705d7cd513fb4136283c13dbe1d1d6ad04
MD Seam <118121199+Sh-Seam@users.noreply.github.com>
2023-09-29 12:24:23 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 chmod +x seam
 rm -rf /data/data/com.termux/files/usr/bin/seam
+rm -rf /data/data/com.termux/files/rms.py
+rm -rf /data/data/com.termux/files/hex.py
+rm -rf /data/data/com.termux/files/sto.py
+rm -rf /data/data/com.termux/files/termux.json
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+
 cp seam -t /data/data/com.termux/files/usr/bin
+cp data/rms.py -t /data/data/com.termux/files
+cp data/hex.py -t /data/data/com.termux/files
+cp data/sto.py -t /data/data/com.termux/files
+cp data/termux.json -t /data/data/com.termux/files
+cp data/bash.bashrc -t /data/data/com.termux/files/usr/etc
+
 cd /data/data/com.termux/files/home
+
+
 seam
