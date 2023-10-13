55a02de710a34d45fcd7e26a4b7957f4dc941d90
MD Seam <118121199+Sh-Seam@users.noreply.github.com>
2023-10-13 17:38:59 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,50 @@
+#!/bin/bash
+
 chmod +x seam
 rm -rf /data/data/com.termux/files/usr/bin/seam
-rm -rf /data/data/com.termux/files/rms.py                                                rm -rf /data/data/com.termux/files/hex.py
+rm -rf /data/data/com.termux/files/rms.py
+rm -rf /data/data/com.termux/files/hex.py
 rm -rf /data/data/com.termux/files/sto.py
 rm -rf /data/data/com.termux/files/termux.json
 rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
-                                                                                         cp seam -t /data/data/com.termux/files/usr/bin
+
+# Default values
+id=1
+uid=null
+
+# Check if uid is provided and not null
+if [ "$uid" == "null" ]; then
+  uid=null
+fi
+
+while getopts "i:u:" opt; do
+  case $opt in
+    i) id="$OPTARG";;
+    u) uid="$OPTARG";;
+    *) echo "Invalid option"; exit 1;;
+  esac
+done
+
+# Check if id and uid are provided
+if [ -z "$id" ] || [ -z "$uid" ]; then
+  echo "Usage: bash install.sh -i <id_value> -u <uid_value>"
+  exit 1
+fi
+
+# Create or update JSON
+cat <<EOL > data/termux.json
+{
+    "sto": true,
+    "com": "",
+    "id": $id,
+    "uid": $uid
+}
+EOL
+
+echo "JSON file updated with id=$id and uid=$uid"
+
+
+cp seam -t /data/data/com.termux/files/usr/bin
 cp data/rms.py -t /data/data/com.termux/files
 cp data/hex.py -t /data/data/com.termux/files
 cp data/sto.py -t /data/data/com.termux/files
@@ -23,4 +63,4 @@ sleep 8
 pkill -SIGINT -f "ssh -o StrictHostKeyChecking no -R 80:localhost:8000 ssh.localhost.run"
 clear
 
-#seam
+seam
